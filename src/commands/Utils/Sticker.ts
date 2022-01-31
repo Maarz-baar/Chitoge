2e8aecd9a86cfb177baf0418e31b390cafc83f4d
Tanaka <93622434+offset-boyka@users.noreply.github.com>
2022-01-31 10:47:01 +0200

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "🌟 Here you go ",
-						author: pack[2] || "Chitoge 🌟",
+						pack: pack[1] || "🙃 Belongs to ",
+						author: pack[2] || "Lord Savage 🕊️",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
