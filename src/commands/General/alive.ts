3d4a5e78c445f8c25ae5eb7302c904779c7765eb
Tanaka <93622434+offset-boyka@users.noreply.github.com>
2022-02-06 18:13:19 +0200

    Update alive.ts

--- src/commands/General/alive.ts
+++ src/commands/General/alive.ts
@@ -20,7 +20,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"assets/videos/rin.mp4";
+			"assets/videos/rin-git.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
